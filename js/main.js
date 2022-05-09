6642a6a3f4839df971153a35376ba6252b6827e2
BeainaT <tania.df@hotmail.it>
2022-05-09 15:05:57 +0200

    added random function + pc numbers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati.
+/* FUNCTIONS */
+//funzione che restituisce numeri casuali in un range
+function getNumberRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+/* MAIN */
+// Dichiaro array da popolare
+let triggerNumber = [];
+// Eseguo ciclo generando un numero random da 1 a 100, per 16 volte.
+while (triggerNumber.length < 16) {
+    let numbers = getNumberRange(1, 100);
+    //SE il numero generato non è presente nell'array, lo aggiungo.
+    if (!triggerNumber.includes(numbers)) {
+        triggerNumber.push(numbers);
+    }
+}
+console.log(triggerNumber);
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
